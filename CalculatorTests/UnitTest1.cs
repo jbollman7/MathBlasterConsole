b3cbdbd5511d1e1f786a85ca873aa85223394424
jumpman <joseph.bollman@gmail.com>
2020-03-20 12:57:49 -0600

    Init commit, unit tests work

---
+++ CalculatorTests/UnitTest1.cs
@@ -0,0 +1,47 @@
+using Calculator;
+using NUnit.Framework;
+
+namespace CalculatorTests
+{
+    public class OperatorTests
+    {
+
+        public enum TestOperatorsList
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
+    
+
+
+    [Test]
+        public void GetOperatorTest()
+        {
+           // Assert.That(TestOperatorsList.Addition, Is.TypeOf();
+        }
+
+
+        [Test]
+        public void AdditionTest()
+        {
+            Assert.AreEqual(4, Operators.Add(2, 2));
+        }
+        [Test]
+        public void SubtractionTest()
+        {
+            Assert.AreEqual(9, Operators.Subtract(13, 4));
+        }
+        [Test]
+        public void MultiplicationTest()
+        {
+            Assert.AreEqual(81, Operators.Multiplication(9, 9));
+        }
+        [Test]
+        public void DivisionTest()
+        {
+            Assert.AreEqual(8, Operators.Division(96, 12));
+        }
+    }
+}
